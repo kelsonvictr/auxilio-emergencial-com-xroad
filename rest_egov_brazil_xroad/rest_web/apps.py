70957aceffc74391aaf9f2d614196b24dd0c7bb1
kelsonvictr <kelson.victr@gmail.com>
2021-10-09 17:07:23 -0300

    Ajustes

--- rest_egov_brazil_xroad/rest_web/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestWebConfig(AppConfig):
-    name = 'rest_web'
--- rest_egov_brazil_xroad/rest_web/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Requisicao_auxilio(models.Model):
-    nome_completo = models.CharField(max_length=1000, blank=True)
-    #dni = models.CharField(max_length=1000, blank=True)
-    #data_nascimento = models.CharField(max_length=1000, blank=True)
